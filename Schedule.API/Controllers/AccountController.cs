6f417c77924a42fd2f869e05eb2e0a04a73e84d5
Efrain Bastidas <mimo4325@gmail.com>
2017-09-16 12:09:03 -0400

    [Schedule.API] Se agrego AccountController, falta probar lo del PermisoAttribute

---
+++ Schedule.API/Controllers/AccountController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Schedule.BLL;
+using Schedule.Entities;
+
+namespace Schedule.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class AccountController : Controller
+    {
+        // GET: api/Account
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Account1", "Account2" };
+        }
+
+        // GET api/Account/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/Account/Login
+        [Route("Login")]
+        [HttpPost]
+        public bool Login([FromBody]Usuario usuario)
+        {
+            return new UsuarioBLL().AuthenticateUser(usuario.Username, usuario.Password);
+        }
+
+        //api/Account/Test
+        [Route("Test")]
+        [HttpGet]
+        public string Test()
+        {
+            return "esto es una prueba";
+        }
+
+        // PUT api/Account/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/Account/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Schedule.API/Filters/PermisoAttribute.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Routing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Schedule.API.Filters
+{
+    public class PermisoAttribute : ActionFilterAttribute
+    {
+        //public RolesPermisos Permiso { get; set; }
+
+        //public override void OnActionExecuting(ActionExecutingContext filterContext)
+        //{
+        //    base.OnActionExecuting(filterContext);
+
+        //    if (!FrontUser.TienePermiso(this.Permiso))
+        //    {
+        //        filterContext.Result = new RedirectToRouteResult(new RouteValueDictionary(new
+        //        {
+        //            controller = "Home",
+        //            action = "Index"
+        //        }));
+        //    }
+        //}
+
+    }
+}
