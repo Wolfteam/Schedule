d4f4c8ed1d909d3d5a115031990333da41103125
Efrain Bastidas <mimo4325@gmail.com>
2017-09-18 22:15:36 -0400

    [Schedule.Web] Se crearon/modificaron metodos en AccountController

--- Schedule.Web/Controllers/AccountController.cs
+++ Schedule.Web/Controllers/AccountController.cs
@@ -1,11 +1,13 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
-using Microsoft.IdentityModel.Protocols;
 using Newtonsoft.Json;
 using Schedule.Entities;
 using Schedule.Web.ViewModels;
 using System;
+using System.Net;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,51 +15,92 @@ namespace Schedule.Web.Controllers
 {
     public class AccountController : Controller
     {
+        #region Variables
         IOptions<AppSettings> _appSettings;
+        static HttpClient _httpClient = null;
+        #endregion
+
+        #region Constructor
         public AccountController(IOptions<AppSettings> appSettings)
         {
             _appSettings = appSettings;
+            if (_httpClient == null)
+            {
+                _httpClient = new HttpClient();
+                InitializeHttpClient(_appSettings.Value.URLBaseAPI);
+            }
         }
+        #endregion
 
         public IActionResult Index()
         {
-            return View();
+            string token = Request.Cookies["Token"];
+            if (String.IsNullOrEmpty(token))
+            {
+                return View();
+            }
+            return RedirectToAction("Index", "Home");
         }
 
         public async Task<ActionResult> Login(LoginViewModel model)
         {
             if (ModelState.IsValid)
             {
-                bool result = false;
-                var client = new HttpClient
-                {
-                    BaseAddress = new Uri(_appSettings.Value.URLBaseAPI)
-                };
-                
-                client.DefaultRequestHeaders.Clear();
-
-                StringContent content = new StringContent(JsonConvert.SerializeObject(new Usuario()
+                HttpResponseMessage response = await _httpClient.PostAsJsonAsync("api/Account/Login", new Usuario()
                 {
                     Username = model.Username,
                     Password = model.Password
-                }), Encoding.UTF8, "application/json");
+                });
 
-                HttpResponseMessage response = await client.PostAsync("api/Account/Login", content);
-
-                //Checking the response is successful or not which is sent using HttpClient  
                 if (response.IsSuccessStatusCode)
                 {
-                    //Storing the response details recieved from web api   
-                    var userResponse = response.Content.ReadAsStringAsync().Result;
-
-                    result = Convert.ToBoolean(userResponse);
-
-                    if (result) return RedirectToAction("Index", "Home");
-                    ModelState.AddModelError("", "Usuario o clave invalidas");
+                    Token token = await response.Content.ReadAsAsync<Token>();
+                    if (token != null)
+                    {
+                        SaveToken(token);
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+                else if (response.StatusCode == HttpStatusCode.NotFound)
+                {
+                    ModelState.AddModelError("", "Fallo la conexion a la API");
                     return View("Index", model);
                 }
+                ModelState.AddModelError("", "Usuario o clave invalidas");
+                return View("Index", model);
             }
-            return View("Index",model);
+            ModelState.AddModelError("", "El modelo no es valido");
+            return View("Index", model);
+        }
+
+        public async Task<ActionResult> Logout()
+        {
+            string token = Request.Cookies["Token"];
+
+            if (!String.IsNullOrEmpty(token))
+            {
+                HttpResponseMessage response = await _httpClient.DeleteAsync(String.Format("api/Account/Logout/{0}", token));
+
+                Response.Cookies.Delete("Token");
+            }
+            return RedirectToAction("Index", "Account");
+        }
+
+        static void InitializeHttpClient(string urlBaseAPI)
+        {
+            _httpClient.BaseAddress = new Uri(urlBaseAPI);
+            _httpClient.DefaultRequestHeaders.Accept.Clear();
+            _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        private void SaveToken(Token token)
+        {
+            CookieOptions cookiesOptions = new CookieOptions
+            {
+                Expires = token.ExpiricyDate
+            };
+            Response.Cookies.Delete("Token");
+            Response.Cookies.Append("Token", token.AuthenticationToken, cookiesOptions);
         }
     }
 }
