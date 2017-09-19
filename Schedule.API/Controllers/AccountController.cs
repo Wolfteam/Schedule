e028d4d23f2cabe947791f9731d69838dacc6053
Efrain Bastidas <mimo4325@gmail.com>
2017-09-18 22:11:00 -0400

    [Schedule.API] Se crearon/corrigieron algunos metodos en AccountController

--- Schedule.API/Controllers/AccountController.cs
+++ Schedule.API/Controllers/AccountController.cs
@@ -1,53 +1,96 @@
-﻿using System.Collections.Generic;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using Schedule.API.Filters;
 using Schedule.BLL;
 using Schedule.Entities;
+using System.Collections.Generic;
 
 namespace Schedule.API.Controllers
 {
     [Route("api/[controller]")]
+    [GlobalAttibute]
     public class AccountController : Controller
     {
-        // GET: api/Account
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "Account1", "Account2" };
-        }
+        #region Variables
+        private readonly ITokenService _tokenServices;
+        private UsuarioBLL _usuario;
+        #endregion
 
-        // GET api/Account/5
-        [HttpGet("{id}")]
-        public string Get(int id)
+        #region Constructor
+        public AccountController(ITokenService tokenServices)
         {
-            return "value";
+            _tokenServices = tokenServices;
+            _usuario = new UsuarioBLL();
         }
+        #endregion
 
         // POST api/Account/Login
-        [Route("Login")]
-        [HttpPost]
-        public bool Login([FromBody]Usuario usuario)
+        [HttpPost("Login")]
+        public ActionResult Login([FromBody]Usuario usuario)
         {
-            return new UsuarioBLL().AuthenticateUser(usuario.Username, usuario.Password);
+            if (_usuario.AuthenticateUser(usuario.Username, usuario.Password))
+            {
+                return Ok(_tokenServices.GenerateToken(usuario.Username));
+            }
+            return Unauthorized();
         }
 
-        //api/Account/Test
-        [Route("Test")]
-        [HttpGet]
-        public string Test()
+        //No ha sido implementado
+        // POST api/Account/Register
+        [HttpPost("Register")]
+        [AuthenticateAttribute]
+        [AuthorizationAttribute(Privilegios.Administrador)]
+        public ActionResult Register([FromBody]Usuario usuario)
         {
-            return "esto es una prueba";
+            //Este metodo pide que esten autenticado y seas admin
+            return Forbid();
         }
 
-        // PUT api/Account/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
+        // DELETE api/Account/123456789
+        [HttpDelete("Logout/{token}")]
+        [AuthenticateAttribute]
+        public ActionResult Logout(string token)
         {
+            if (_tokenServices.DeleteToken(token))
+            {
+                return Ok();
+            }
+            return NotFound();
         }
 
-        // DELETE api/Account/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
+        //GET api/Account/123456798
+        [HttpGet("Privilegios/{token}")]
+        [AuthenticateAttribute]
+        public List<Privilegios> GetAllPrivilegiosByToken(string token)
         {
+            return _tokenServices.GetAllPrivilegiosByToken(token);
         }
     }
 }
+
+
+// GET: api/Account
+//[HttpGet]
+//public IEnumerable<string> Get()
+//{
+//    return new string[] { "Account1", "Account2" };
+//}
+
+//// GET api/Account/5
+//[HttpGet("{id}")]
+//public string Get(int id)
+//{
+//    return "value";
+//}
+
+
+//// PUT api/Account/5
+//[HttpPut("{id}")]
+//public void Put(int id, [FromBody]string value)
+//{
+//}
+
+//// DELETE api/Account/5
+//[HttpDelete("{id}")]
+//public void Delete(int id)
+//{
+//}
