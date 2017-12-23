6b0d13b0d22c50ea9b00cb6fc243c79e43242f9c
Efrain Bastidas <mimo4325@gmail.com>
2017-12-23 12:59:27 -0400

    [Schedule.Web] Creado repositorio base para todos los demas

---
+++ Schedule.Web/Models/Repository/Repository.cs
@@ -0,0 +1,80 @@
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Schedule.Web.Models.Repository
+{
+    public class Repository<TEntity, TEntityDetails> : IRepository<TEntity, TEntityDetails>
+        where TEntity : class
+        where TEntityDetails : class
+    {
+        protected readonly HttpClient _httpClient;
+        protected readonly string _urlEntityApi;
+
+        public Repository(HttpClient httpClient, string urlEntityApi)
+        {
+            _httpClient = httpClient;
+            _urlEntityApi = urlEntityApi;
+        }
+
+        public async Task<bool> AddAsync(TEntity entity)
+        {
+            HttpResponseMessage response = await _httpClient.PostAsJsonAsync(_urlEntityApi, entity);
+            if (response.IsSuccessStatusCode)
+                return true;
+            else
+                return false;
+        }
+
+        public async Task<bool> AddRangeAsync(IEnumerable<TEntity> entity)
+        {
+            HttpResponseMessage response = await _httpClient.PostAsJsonAsync(_urlEntityApi, entity);
+            if (response.IsSuccessStatusCode)
+                return true;
+            else
+                return false;
+        }
+
+        public async Task<TEntityDetails> GetAsync(object id)
+        {
+            TEntityDetails entity = null;
+            HttpResponseMessage response = await _httpClient.GetAsync($"{_urlEntityApi}/{id}");
+            if (response.IsSuccessStatusCode)
+            {
+                entity = await response.Content.ReadAsAsync<TEntityDetails>();
+            }
+            return entity;
+        }
+
+        public async Task<List<TEntityDetails>> GetAllAsync()
+        {
+            List<TEntityDetails> entities = new List<TEntityDetails>();
+            HttpResponseMessage response = await _httpClient.GetAsync(_urlEntityApi);
+            if (response.IsSuccessStatusCode)
+                entities = await response.Content.ReadAsAsync<List<TEntityDetails>>();
+            return entities;
+        }
+
+        public async Task<bool> RemoveAsync(object id)
+        {
+            HttpResponseMessage response = await _httpClient.DeleteAsync($"{_urlEntityApi}/{id}");
+            if (response.IsSuccessStatusCode)
+                return true;
+            else
+                return false;
+        }
+
+        public async Task<bool> UpdateAsync(object id, TEntity entity)
+        {
+            HttpResponseMessage response = await _httpClient.PutAsJsonAsync($"{_urlEntityApi}/{id}", entity);
+            if (response.IsSuccessStatusCode)
+                return true;
+            else
+                return false;
+        }
+    }
+}
