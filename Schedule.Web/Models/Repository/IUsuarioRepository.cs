6a7ab31acf3abd86451901ead20e1025aae25a14
Efrain Bastidas <mimo4325@gmail.com>
2018-06-08 21:58:32 -0400

    Agregado controlador y metodos correspondientes para el cambio de password

--- Schedule.Web/Models/Repository/IUsuarioRepository.cs
+++ Schedule.Web/Models/Repository/IUsuarioRepository.cs
@@ -1,8 +1,16 @@
+using System.Threading.Tasks;
 using Schedule.Entities;
 
 namespace Schedule.Web.Models.Repository
 {
     public interface IUsuarioRepository : IRepository<UsuarioDTO, UsuarioDetailsDTO>
     {
+        /// <summary>
+        /// Cambia la password a un usuario en particular
+        /// </summary>
+        /// <param name="cedula">Cedula del usuario</param>
+        /// <param name="request">Objeto que contiene la password vieja y nueva</param>
+        /// <returns>ResultDTO con el resultado de la operacion</returns>
+        Task<ResultDTO> ChangePassword(uint cedula, ChangePasswordDTO request);
     }
 }
